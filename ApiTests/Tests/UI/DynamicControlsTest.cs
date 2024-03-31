7da92d2949e211e570a887235d4ca6b735b6d1fc
Mihail Matskevich <m.matskevich@a1qa.com>
2024-03-31 12:45:04 +0300

    updated

--- ApiTests/Tests/UI/DynamicControlsTest.cs
+++ ApiTests/Tests/UI/DynamicControlsTest.cs
@@ -8,14 +8,22 @@ namespace TestProject.Tests.UI
     internal class DynamicControlsTests : BaseTest
     {
         private static readonly By enableBtn = By.XPath(string.Format(XpathPatterns.preciseTextXpath, "Enable"));
+        MainPage mainPage = new MainPage();
+        WebDriver driver = Browser.GetDriver();
+
 
         [Test]
         public void DynamicControlsTest()
         {
-            MainPage mainPage = new MainPage();
             mainPage.ClickOnDynamicControl();
             Browser.GetDriver().FindElement(enableBtn).Click();
+            //WebDriverWait wait = new WebDriverWait(Browser.GetDriver(), TimeSpan.FromSeconds(maxWait));
+            //IWebElement inputElement = wait.Until(ExpectedConditions.ElementToBeClickable(inputLocator));
             //assert input is enabled
+            //assert input is disabled
+
+
+
             //input randomly generated text
             //assert input text
         }
