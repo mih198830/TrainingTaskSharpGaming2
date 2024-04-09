11d9a35c937019d2c0bb71923fc1bcd47355cbda
Mihail Matskevich <m.matskevich@a1qa.com>
2024-04-09 13:51:31 +0300

    page object changes for DynamicControlTest

--- ApiTests/Tests/Pages/MainPage.cs
+++ ApiTests/Tests/Pages/MainPage.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +12,48 @@ namespace TestProject.Tests.Pages
 {
     internal class MainPage
     {
-        private static readonly By dynamicControl = By.XPath(string.Format(XpathPatterns.preciseTextXpath, "Dynamic Controls"));
+        private static readonly By inputField = By.XPath(string.Format(XpathPatterns.typeText));
+        private static readonly By dynamicControl = By.XPath(string.Format(XpathPatterns.preciseTextXpath, 
+            "Dynamic Controls"));
+        private static readonly By enableBtn = By.XPath(string.Format(XpathPatterns.preciseTextXpath, "Enable"));
+        private static readonly By disableBtn = By.XPath(string.Format(XpathPatterns.preciseTextXpath, "Disable"));
+        WebDriverWait wait = new WebDriverWait(Browser.GetDriver(), TimeSpan.FromSeconds(100));
+        
 
         public void ClickOnDynamicControl()
         {
             Browser.GetDriver().FindElement(dynamicControl).Click();
+        }
+
+        public void ClickOnEnableBtn()
+        {
+            Browser.GetDriver().FindElement(enableBtn).Click();
+        }
+
+        public void WaitForEnableBtn()
+        {
+            wait.Until(ExpectedConditions.ElementToBeClickable(enableBtn));
+        }
+
+        public void WaitForDisableBtn()
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(disableBtn));
+        }
+
+        public void EnterTextInInputField(string randomValue)
+        {
+            Browser.GetDriver().FindElement(inputField).SendKeys(randomValue);
+        }
+
+        public string GetAttributeOfInputField()
+        {
+            return Browser.GetDriver().FindElement(inputField).GetAttribute("value");
+        }
+
+        public bool CheckIfButtonIsEnabled()
+        {
+            _ = Browser.GetDriver().FindElement(disableBtn).Enabled;
+            return true;
         }
     }
 }
--- ApiTests/Tests/UI/DynamicControlsTest.cs
+++ ApiTests/Tests/UI/DynamicControlsTest.cs
@@ -1,6 +1,5 @@
 ﻿using ApiTests.Tests.UI;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using TestProject.Tests.Pages;
 using TestProject.Utils;
@@ -10,24 +9,20 @@ namespace TestProject.Tests.UI
     internal class DynamicControlsTests : BaseTest
     {
         readonly MainPage mainPage = new MainPage();
-        private static readonly By enableBtn = By.XPath(string.Format(XpathPatterns.preciseTextXpath, "Enable"));
-        private static readonly By disableBtn = By.XPath(string.Format(XpathPatterns.preciseTextXpath, "Disable"));
-        private static readonly By inputField = By.XPath(string.Format(XpathPatterns.typeText));
+        
         private static readonly string randomValue = Guid.NewGuid().ToString();
 
         [Test]
         public void DynamicControlsTest()
         {
             mainPage.ClickOnDynamicControl();
-            WebDriverWait wait = new WebDriverWait(Browser.GetDriver(), TimeSpan.FromSeconds(100));
-            wait.Until(ExpectedConditions.ElementToBeClickable(enableBtn));
-            Browser.GetDriver().FindElement(enableBtn).Click();
-            wait.Until(ExpectedConditions.ElementIsVisible(disableBtn));
+            mainPage.ClickOnEnableBtn();
+            mainPage.WaitForDisableBtn();
             //assert input is enabled
-            Assert.That(Browser.GetDriver().FindElement(disableBtn).Enabled, Is.True);
+            Assert.That(mainPage.CheckIfButtonIsEnabled, Is.True);
             //input randomly generated text
-            Browser.GetDriver().FindElement(inputField).SendKeys(randomValue);
-            string printedText = Browser.GetDriver().FindElement(inputField).GetAttribute("value");
+            mainPage.EnterTextInInputField(randomValue);
+            string printedText = mainPage.GetAttributeOfInputField();
             //assert input text
             Assert.That(randomValue, Is.EqualTo(printedText), $"Printed text '{printedText}' is not" +
                 $" what was inputted '{randomValue}'");
